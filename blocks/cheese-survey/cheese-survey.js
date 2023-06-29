44153eceeddd4d09c13b79145df4b8052d61f842
sghimpos <sghimpos@adobe.com>
2023-06-29 16:07:51 +0300

    Hello block

--- blocks/cheese-survey/cheese-survey.js
+++ blocks/cheese-survey/cheese-survey.js
@@ -9,7 +9,7 @@ export default async function init(el) {
   const french = createTag('option', { value: 'french'}, 'French');
   const romanian = createTag('option', { value: 'romanian'}, 'Romanian');
   const submit = createTag('button', { class: 'cheese-form-submit'}, 'Send your preference');
-  select.append(swiss, french);
+  select.append(swiss, french, romanian);
   form.append(label, select, submit);
   el.append(form);
 
